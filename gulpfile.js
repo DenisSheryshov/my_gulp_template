31cd541bb1929d8e2e56069852a256173a31356b
Denis Sheryshov <denis.sheryshov@gmail.com>
2021-04-02 18:36:03 +0500

    compression plugins

--- gulpfile.js
+++ gulpfile.js
@@ -4,44 +4,71 @@ const concat = require('gulp-concat')
 const sass = require('gulp-sass')
 const autoprefixer = require('gulp-autoprefixer')
 const cleanCss = require('gulp-clean-css')
-const gulpIf = require('gulp-if')
+const htmlmin = require('gulp-htmlmin')
+const uglify = require('gulp-uglify-es').default;
 const server = require('browser-sync').create()
 
 const config = {
+	isDevelop: true,
 	paths: {
 		scss: './src/scss/**/*.scss',
-		html: './dist/index.html'
+		html: './app/index.html',
+		html_min: './dist/index.html'
 	},
 	output: {
-		cssName: 'bundle.min.css',
-		path: './dist'
-	},
-	isDevelop: true
+		cssName: 'bundle.css',
+		path() {
+			return config.isDevelop ? './app' : './dist'
+		}
+	}	
 }
 
-const scss = function() {
+const scss2css = function() {
 	return gulp.src(config.paths.scss)
-		.pipe(gulpIf(config.isDevelop, soucemaps.init()))
+		.pipe(soucemaps.init())
 		.pipe(sass())
 		.pipe(concat(config.output.cssName))
 		.pipe(autoprefixer())
-		.pipe(gulpIf(!config.isDevelop, cleanCss()))
-		.pipe(gulpIf(config.isDevelop, soucemaps.write()))
-		.pipe(gulp.dest(config.output.path))
+		.pipe(soucemaps.write())
+		.pipe(gulp.dest('./app'))
 		.pipe(server.stream())
 }
 
+
+const compressCSS = function() {
+	return gulp.src('./app/*.css')
+		.pipe(cleanCss())
+		.pipe(gulp.dest('./dist'))
+}
+
+const compressHTML = function() {
+	return gulp.src('./app/*.html')
+		.pipe(htmlmin({
+			collapseWhitespace: true,
+			removeComments: true}))
+		.pipe(gulp.dest('./dist'))
+}
+
+const compressJS = function() {
+   return gulp.src('./app/*.js')
+        .pipe(uglify({}))
+        .pipe(gulp.dest('./dist'))
+}
+
+
 const serve = function() {
     server.init({
-        server: config.output.path,
+        server: config.output.path(),
         notify: false,
         open: true,
         cors: true
     })
 
-    gulp.watch(config.paths.scss, gulp.series(scss))
+    gulp.watch(config.paths.scss, gulp.series(scss2css))
     gulp.watch(config.paths.html).on('change', server.reload)
 }
 
-exports.default = gulp.series(scss, serve)
+exports.default = gulp.series(scss2css, serve)
+exports.min = gulp.series(compressHTML, compressCSS, compressJS)
+
 
